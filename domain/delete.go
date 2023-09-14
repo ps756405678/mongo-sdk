22a89aca3c6ba434b24b7dd2e50196f992276106
ps120 <ps120_studio@outlook.com>
2023-09-14 16:00:14 +0800

    sdk update

--- domain/delete.go
+++ domain/delete.go
@@ -1,6 +1,13 @@
 package domain
 
+import "encoding/json"
+
 type DeleteWrapper struct {
 	CallSdkReq
 	Data []string `json:"data"`
+}
+
+func (req *DeleteWrapper) ToJson() []byte {
+	bdata, _ := json.Marshal(req)
+	return bdata
 }
