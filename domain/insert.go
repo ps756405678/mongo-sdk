95636ef407210df14f035a30061308dc84395323
ps120 <ps120_studio@outlook.com>
2023-10-20 18:02:13 +0800

    insert option

--- domain/insert.go
+++ domain/insert.go
@@ -4,12 +4,14 @@ import "encoding/json"
 
 type InsertOneReq[T any] struct {
 	CallSdkReq
-	Data T `json:"data"`
+	InsertOption string `json:"insert_option"` // abort / replace
+	Data         T      `json:"data"`
 }
 
 type InsertManyReq[T any] struct {
 	CallSdkReq
-	Data []T `json:"data"`
+	InsertOption string `json:"insert_option"` // abort / replace / error
+	Data         []T    `json:"data"`
 }
 
 func (req *InsertOneReq[T]) ToJson() []byte {
@@ -20,4 +22,19 @@ func (req *InsertOneReq[T]) ToJson() []byte {
 func (req *InsertManyReq[T]) ToJson() []byte {
 	bdata, _ := json.Marshal(req)
 	return bdata
+}
+
+func (req *InsertManyReq[T]) AbortWhenDuplicateKey() *InsertManyReq[T] {
+	req.InsertOption = "abort"
+	return req
+}
+
+func (req *InsertManyReq[T]) ReplaceWhenDuplicateKey() *InsertManyReq[T] {
+	req.InsertOption = "replace"
+	return req
+}
+
+func (req *InsertManyReq[T]) ErrorWhenDuplicateKey() *InsertManyReq[T] {
+	req.InsertOption = "error"
+	return req
 }
